43b16a6f8b1fa9fe2f0250d359abd8bcff3e496f
AlFaOm <al_fa_omega@mail.ru>
2022-06-28 00:46:23 +0700

    Finished

--- source/js/modules/show-card-coaches.js
+++ source/js/modules/show-card-coaches.js
@@ -8,7 +8,7 @@ const TAB_WIDTH = 1199;
 const MOB_WIDTH = 767;
 const DESKTOP_WIDTH = 1200;
 
-function myCoolFunction() {
+function toggleCardCoaches() {
   const windowInnerWidth = document.documentElement.clientWidth;
   const i = Array.prototype.indexOf.call(coachesSlide, this);
   if (windowInnerWidth < DESKTOP_WIDTH) {
@@ -19,16 +19,6 @@ function myCoolFunction() {
       coachesCardInfo[i].classList.add("coaches__card-info--tab-show");
 
       headingSlide[i].style.display = "none";
-      // headingSlide[i].style.bottom = "";
-      // headingSlide[i].style.top = "52px";
-      // headingSlide[i].style.zIndex = 2;
-      // headingSlide[i].style.left = "48%";
-
-      // if (windowInnerWidth <= MOB_WIDTH) {
-      //   headingSlide[i].style.top = "42px";
-      //   headingSlide[i].style.zIndex = 2;
-      //   headingSlide[i].style.left = "50%";
-      // }
     } else if (
       windowInnerWidth < DESKTOP_WIDTH &&
       coachesCardInfo[i].classList.contains("coaches__card-info--tab-show")
@@ -36,16 +26,13 @@ function myCoolFunction() {
       coachesCardInfo[i].classList.remove("coaches__card-info--tab-show");
 
       headingSlide[i].style.display = "block";
-      // headingSlide[i].style.top = "";
-      // headingSlide[i].style.bottom = "22px";
-      // headingSlide[i].style.left = "50%";
     }
   }
 }
 
 export const showCardCoaches = () => {
   coachesSlide.forEach(function (tab) {
-    tab.removeEventListener("click", myCoolFunction);
-    tab.addEventListener("click", myCoolFunction);
+    tab.removeEventListener("click", toggleCardCoaches);
+    tab.addEventListener("click", toggleCardCoaches);
   });
 };
